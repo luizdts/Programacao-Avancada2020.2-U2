a0dd576044b82b47642fe9ee501fed3d709a3a31
Luiz Henrique Araujo Dantas <77617519+luizdts@users.noreply.github.com>
2021-03-18 15:29:09 -0300

    Update main.cpp

--- projetoescultor/parte 1/main.cpp
+++ projetoescultor/parte 1/main.cpp
@@ -33,8 +33,37 @@ int main(void){
     v3->cutBox(18,20,21,21,15,16);
 
     v3->setColor(255,255,255,1);
-    v3->setColor(250,250,250,0);
     v3->putSphere(12,2,9,0.5); 
+   
+    v3->setColor(0,0,0,0);
+    v3->cutBox(16,16,1,1,15,15); // buraco
+
+    v3->putBox(46,48,0,9,25,25);
+    v3->putBox(46,48,0,9,29,29); // bolsa de golfe
+    v3->putBox(45,45,0,9,25,29);
+    v3->putBox(49,49,0,9,25,29);
+
+    v3->cutBox(49,49,2,9,25,25);
+    v3->cutBox(49,49,2,9,29,29);
+    v3->cutBox(45,45,2,9,25,25); // molde da bolsa de golfe
+    v3->cutBox(45,45,2,9,29,29);
+
+    v3->putBox(46,46,0,12,26,26); // tacos da bolsa
+    v3->putBox(48,48,0,12,27,27);
+
+    v3->putSphere(45,12,26,1); //bolinha de teste
+    v3->putSphere(49,12,27,1); //bolinha de teste
+    v3->putBox(44,44,11,11,26,26);  // preenchimentos do taco
+    v3->putBox(49,50,11,11,27,27);
+
+    v3->cutBox(49,49,12,12,26,26);
+    v3->cutBox(49,49,12,12,28,28);
+    v3->cutBox(49,49,13,13,27,27);  // molde dos tacos
+
+    v3->cutBox(45,45,12,12,25,25);
+    v3->cutBox(45,45,12,12,27,27);
+    v3->cutBox(45,45,13,13,26,26);  // molde dos tacos
+
 
     v3->writeOFF((char*)"arquivoteste.off");
 
