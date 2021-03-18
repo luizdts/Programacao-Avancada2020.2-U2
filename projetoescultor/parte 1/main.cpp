0b418afb2b001f479631a456c0dcefacaf90b854
Daniel <niel.davi@hotmail.com>
2021-03-18 13:37:25 -0300

    Add files via upload

--- projetoescultor/parte 1/main.cpp
+++ projetoescultor/parte 1/main.cpp
@@ -1,14 +1,12 @@
+
+#include "escultor.cpp"
 #include <iostream>
 #include <fstream>
-#include <sstream>
 #include <string>
-#include <vector>
-#include "escultor.h"
+#include <cstdlib>
 
-using namespace std;
-
-int main()
-{
+int main(void){
+   
     Escultor *v3;
     int a = 120, b= 120, c = 120;
 
@@ -16,7 +14,12 @@ int main()
     
     v3->setColor(0,255,0,1);
     v3->putBox(0,50,0,1,0,30); // piso
+      
+    v3->setColor(255,255,255,1);
+
     v3->putBox(15,15,0,15,15,15); // cabo da bandeira
+    
+    v3->setColor(255,0,0,1);
     v3->putBox(15,20,15,21,15,15); // bandeira
 
     v3->cutBox(18,20,15,15,15,16);
@@ -28,12 +31,13 @@ int main()
     v3->cutBox(20,20,20,21,15,16); // cortes superiores
     v3->cutBox(19,20,20,21,15,16);
     v3->cutBox(18,20,21,21,15,16);
-   
+
+    v3->setColor(255,255,255,1);
     v3->setColor(250,250,250,0);
-    v3->putSphere(12,2,9,0.5); //bolinha de golfe
+    v3->putSphere(12,2,9,0.5); 
 
-    v3->writeOFF("arquivoteste.off");
+    v3->writeOFF((char*)"arquivoteste.off");
 
-  return 0; 
+
 
 }
