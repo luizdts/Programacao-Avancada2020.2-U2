4ff1696d1b602a8b5e5b5f74197a5cb55796cf56
Luiz Henrique Araujo Dantas <77617519+luizdts@users.noreply.github.com>
2021-03-18 10:57:32 -0300

    Update main.cpp

--- projetoescultor/parte 1/main.cpp
+++ projetoescultor/parte 1/main.cpp
@@ -13,12 +13,22 @@ int main()
     int a = 120, b= 120, c = 120;
 
     v3 = new Escultor(a,b,c);
-
+    
     v3->setColor(0,255,0,1);
     v3->putBox(0,30,0,1,0,30);
     v3->putBox(15,15,0,15,15,15);
-    v3->putBox(15,20,15,20,15,15);
+    v3->putBox(15,20,15,21,15,15);
 
+    v3->cutBox(18,20,15,15,15,16);
+    v3->cutBox(19,20,16,16,15,16); // cortes inferiores
+    v3->cutBox(20,20,17,17,15,16);
+    v3->cutBox(20,20,19,20,15,16);
+
+    v3->cutBox(19,20,20,21,15,16);
+    v3->cutBox(19,19,20,21,15,16);
+    v3->cutBox(20,20,20,21,15,16); // cortes superiores
+    v3->cutBox(19,20,20,21,15,16);
+    v3->cutBox(18,20,21,21,15,16);
 
     v3->writeOFF("arquivoteste.off");
 
