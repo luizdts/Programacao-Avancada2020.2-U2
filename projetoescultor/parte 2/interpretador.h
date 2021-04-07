2826e8ce6f9006779eeb79222d5239e609514789
Luiz Henrique Araujo Dantas <77617519+luizdts@users.noreply.github.com>
2021-04-07 09:37:37 -0300

    header do interpretador

---
+++ projetoescultor/parte 2/interpretador.h
@@ -0,0 +1,26 @@
+#ifndef INTERPRETADOR_H
+#define INTERPRETADOR_H
+#include "figurageometrica.h"
+#include "escultor.h"
+#include <vector>
+#include <string>
+
+class Interpretador
+{
+    int x, y, z;
+    float r, g, b, a;
+
+public:
+    Interpretador();
+    std::vector<FiguraGeometrica*> parte (std::string filename);
+
+    int getX();
+
+    int getY();
+
+    int getZ();
+
+
+};
+
+#endif // INTERPRETADOR_H
